0b283419088547e59f94a2410f0e48125ef314f2
Shashank <mishtashashank31@gmail.com>
2022-02-10 17:23:12 +0530

    Ray added

---
+++ Assets/Scripts/Ray.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray : MonoBehaviour
+{
+    LineRenderer ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ray = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray.SetPosition(0, transform.position);
+        ray.SetPosition(1, transform.forward * 5);
+    }
+}
