a0ca4d60fa665b41273b0fe5463da1f7071d30bc
SUMIT KUMAR <sumit.raipura@gmail.com>
2022-04-10 10:05:31 +0530

    Initial Commit
    Reflection of ray

--- Assets/Scripts/Ray.cs
+++ Assets/Scripts/Ray.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Ray : MonoBehaviour
-{
-    LineRenderer ray;
+public class Ray : MonoBehaviour {
+    public float raycastLength = 100f;
+    public float reflectLength = 5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        ray = GetComponent<LineRenderer>();
+    [Header("Optional")]
+    public LineRenderer lineRay;
+
+    private void Awake() {
+        if (lineRay == null) lineRay = GetComponent<LineRenderer>();
+        lineRay.positionCount = 3;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        ray.SetPosition(0, transform.position);
-        ray.SetPosition(1, transform.forward * 5);
+    void Update() {
+        lineRay.SetPosition(0, transform.position);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, raycastLength)) {
+            lineRay.SetPosition(1, hit.point);
+            Vector3 reflectDirection = Vector3.Reflect(transform.forward, hit.normal);
+            lineRay.SetPosition(2, hit.point + reflectDirection * reflectLength);
+        }else
+        {
+            lineRay.SetPosition(1, transform.position);
+            lineRay.SetPosition(2, transform.forward * raycastLength);
+        }
     }
 }
